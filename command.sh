39c773ed29a122249f78d1e0fd700081324ec0bd
mor bargig <morbargig@gmail.com>
2020-07-31 18:04:34 +0300

    "automatic commit" pull && push by mor at Fri 31 Jul 2020 18:04:34

--- command.sh
+++ command.sh
@@ -5,6 +5,7 @@ whoami=`whoami`
 git checkout $branchName
 commit="automatic commit"
 if [ "$1" ]; then commit="$1"; fi
+git add .
 git commit -am "\"$commit\" pull && push by $whoami at ${date::${#date}-4}"
 git pull origin $branchName
 git add .
